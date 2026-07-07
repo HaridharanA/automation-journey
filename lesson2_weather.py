6ef43c645022666e5343a7e189ad5aea0b20f31f
Haridharan <haridharan36829@gmail.com>
2026-07-08 00:21:16 +0530

    ADD wind speed and feels like

--- lesson2_weather.py
+++ lesson2_weather.py
@@ -14,11 +14,17 @@ if response.status_code == 200:
     temp = data["main"]["temp"]
     humidity = data["main"]["humidity"]
     description = data["weather"][0]["description"]
+    wind_speed = data["wind"]["speed"]
+    feels_like = data["main"]["feels_like"]
+
 
 
     print(f"City: {city}")
     print(f"Temperature: {temp}°C")
     print(f"Humidity: {humidity}%")
     print(f"Condition: {description}")
+    print(f"wind speed: {wind_speed}")
+    print(f"feels like: {feels_like}")
+
 else:
     print(f"Error: {response.status_code}")
